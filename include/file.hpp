09e4ffb12a464dc11d3518dc3fca5307fd753d54
ppaabo <89841937+ppaabo@users.noreply.github.com>
2026-01-22 18:27:23 +0200

    Add stat_ member + retrieve_attributes() and get_size() to File

--- include/file.hpp
+++ include/file.hpp
@@ -1,13 +1,19 @@
 #pragma once
 
 #include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 class File {
 public:
   File(const std::string &name, const std::string &path);
   const std::string &get_name() const;
+  off_t get_size() const;
 
 private:
   std::string name_;
   std::string full_path_;
+  struct stat stat_;
+
+  void retrieve_attributes();
 };
--- src/file.cpp
+++ src/file.cpp
@@ -1,7 +1,25 @@
 #include "file.hpp"
+#include <cstring>
+#include <stdexcept>
 #include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 File::File(const std::string &name, const std::string &dir)
-    : name_(name), full_path_(dir + "/" + name) {}
+    : name_(name), full_path_(dir + "/" + name), stat_{} {
+  retrieve_attributes();
+}
 
 const std::string &File::get_name() const { return name_; }
+off_t File::get_size() const { return stat_.st_size; }
+
+void File::retrieve_attributes() {
+  struct stat buf;
+  int result = stat(full_path_.c_str(), &buf);
+
+  if (result == -1) {
+    throw std::runtime_error(std::string("stat failed for ") + full_path_ +
+                             ": " + strerror(errno));
+  }
+  stat_ = buf;
+}
