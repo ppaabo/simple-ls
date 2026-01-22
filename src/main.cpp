2081b8bd3129e849b17f244479230345a65710a8
ppaabo <89841937+ppaabo@users.noreply.github.com>
2026-01-22 18:29:24 +0200

    Add print_file_info method to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include "directory.hpp"
+#include "file.hpp"
 #include <exception>
 #include <iostream>
+
+void print_file_info(const File &file) {
+  std::cout << file.get_name() << " " << file.get_size() << "\n";
+}
 
 int main(int argc, char *argv[]) {
   try {
     Directory dir(".");
     for (const auto &file : dir.get_entries()) {
-      std::cout << file.get_name() << "\n";
+      print_file_info(file);
     }
   } catch (const std::exception &e) {
     std::cerr << "error occured:  " << e.what() << "\n";
