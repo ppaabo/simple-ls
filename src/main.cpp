d3d90220b63544214d08b1e1ace6647882f86849
ppaabo <89841937+ppaabo@users.noreply.github.com>
2026-01-24 17:56:49 +0200

    Update main to use argparse

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
+#include "argparse.hpp"
 #include "directory.hpp"
 #include "file.hpp"
 #include <exception>
+#include <filesystem>
 #include <iostream>
+namespace fs = std::filesystem;
 
 void print_file_info(const File &file) {
   std::cout << file.get_type() << file.get_permissions() << " "
@@ -10,12 +13,30 @@ void print_file_info(const File &file) {
 }
 
 int main(int argc, char *argv[]) {
+  argparse::ArgumentParser program("simple-ls");
+  program.add_argument("path").help("Directory path").default_value(".");
+
   try {
-    Directory dir(".");
+    program.parse_args(argc, argv);
+  } catch (const std::exception &e) {
+    std::cerr << "error occured during argument parsing: " << e.what() << "\n";
+    std::cerr << program;
+    return 1;
+  }
+
+  try {
+    std::string path = program.get<std::string>("path");
+    if (!fs::exists(path) || !fs::is_directory(path)) {
+      throw std::runtime_error("provided path is invalid: " + path);
+    }
+    Directory dir(path);
     for (const auto &file : dir.get_entries()) {
       print_file_info(file);
     }
   } catch (const std::exception &e) {
-    std::cerr << "error occured:  " << e.what() << "\n";
+    std::cerr << "error occured: " << e.what() << "\n";
+    return 1;
   }
+
+  return 0;
 }
